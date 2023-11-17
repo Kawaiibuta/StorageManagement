ff6a8ede98792db1d195a92fedaf0c0ed67eef42
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2023-11-17 10:53:16 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import AppHeader from "./components/AppHeader";
 import Authentication from "./pages/Authentication";
 function App() {
   return (
-    // <div className="App">
-    //   <AppHeader />
-    //   <div className="SideMenuAndPageContent">
-    //     <NavBar />
-    //     <PageContent />
-    //   </div>
-    // </div>
-    <Authentication></Authentication>
+    <div className="App">
+      <AppHeader />
+      <div className="SideMenuAndPageContent">
+        <NavBar />
+        <PageContent />
+      </div>
+    </div>
+    // <Authentication></Authentication>
   );
 }
 export default App;
