84b62a83755ae91c3f1c8dea77b13ef14ada5c10
unknown <dangkhoa141003@gmail.com>
2023-11-17 21:13:10 +0700

    Form UI

---
+++ src/components/Form/NewCustomerForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Form,Input, Button} from 'antd';
+
+const { TextArea } = Input;
+
+function NewCustomerForm() {
+  return (
+    <>
+    <div>
+      <h3>New Customer</h3>
+     <Form 
+     labelCol={{ span: 15 }}
+     wrapperCol={{ span: 30 }}
+     layout="horizontal"
+     style={{ Width: 1800 }}>
+        <Form.Item label="Customer Name:">
+            <Input />
+        </Form.Item>
+        <Form.Item label="Address: ">
+            <TextArea rows={4}/>
+        </Form.Item>
+        <Form.Item label="PhoneNumber:">
+          <Input type='phone'/>
+        </Form.Item>
+        <Form.Item label="Email:">
+          <Input type='email'/>
+        </Form.Item>
+        <Form.Item style={{ paddingLeft: 100}}>
+          <Button >Submit</Button>
+        </Form.Item>
+        </Form>
+    </div>
+    </>
+  )
+}
+
+export default NewCustomerForm  
---
+++ src/components/Form/NewSupplierForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Form,Input, Button} from 'antd';
+
+const { TextArea } = Input;
+
+function NewSupplierForm() {
+  return (
+    <>
+    <div >
+      <h3>New Supplier</h3>
+     <Form 
+     labelCol={{ span: 15 }}
+     wrapperCol={{ span: 30 }}
+     layout="horizontal"
+     style={{ Width: 1800 }}>
+        <Form.Item label="Supplier Name:">
+            <Input />
+        </Form.Item>
+        <Form.Item label="Address: ">
+            <TextArea rows={4}/>
+        </Form.Item>
+        <Form.Item label="PhoneNumber:">
+          <Input type='phone'/>
+        </Form.Item>
+        <Form.Item label="Email:">
+          <Input type='email'/>
+        </Form.Item>
+        <Form.Item style={{ paddingLeft: 100}}>
+          <Button >Submit</Button>
+        </Form.Item>
+        </Form>
+    </div>
+    </>
+  )
+}
+
+export default NewSupplierForm
